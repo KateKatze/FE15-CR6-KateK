651f558d1741dc3a6b39e110cf4ca148aa748f88
KateKatze <dr.katekogler@gmail.com>
2022-02-18 11:07:39 +0100

    routing created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { MenuComponent } from './menu/menu.component';
+import { CartComponent } from './cart/cart.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
+  path: 'aboutus',
+  component: AboutusComponent
+}, {
+  path: 'menu',
+  component: MenuComponent
+}, {
+  path: 'cart',
+  component: CartComponent
+}, {
+  path: 'details',
+  component: DetailsComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AboutusComponent } from './aboutus/aboutus.component';
 import { MenuComponent } from './menu/menu.component';
 import { DetailsComponent } from './details/details.component';
 import { CartComponent } from './cart/cart.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CartComponent } from './cart/cart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
